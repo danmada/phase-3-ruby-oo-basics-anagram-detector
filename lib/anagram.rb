63c0665b35b1f3e1450cb0c7958594fab6b6a72a
danmada <danmada@gmail.com>
2021-07-21 17:04:20 -0500

    commit tree 273c021412dc580733771caea4981f6fe55e9d63

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match
+        word_array = @word.chars
+        binding.pry
+    end
+
+end
+
+match
+
